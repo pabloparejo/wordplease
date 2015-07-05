a418db4e993ff765eadf43b40ef34dd870226ff7
pabloparejo <parejo.p@gmail.com>
2015-07-05 14:49:17 +0200

    SignupForm inheritage

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,6 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label='Username')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
 
 class SignUpForm(UserCreationForm):
-    class Meta:
-        model = User
+    class Meta(UserCreationForm.Meta):
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
