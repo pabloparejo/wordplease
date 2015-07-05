202f0d376e652592983a5d09966f07f3c2be439a
pabloparejo <parejo.p@gmail.com>
2015-07-05 14:50:02 +0200

    frontend login

--- users/views.py
+++ users/views.py
@@ -1,43 +1,29 @@
 #encoding:UTF-8
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
-from django.contrib.auth.models import User
-from django.views.generic import View, FormView, ListView
+from django.views.generic import FormView
 
-from forms import LoginForm, SignUpForm
-from django.contrib.auth.forms import UserCreationForm
+from forms import SignUpForm
 
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = AuthenticationForm
+    success_url = reverse_lazy("home")
 
-class LoginView(View):
+    def form_valid(self, form):
+        username = form.cleaned_data.get("username", "")
+        password = form.cleaned_data.get("password", "")
+        user = authenticate(username=username, password=password)
+        if user:
+            django_login(self.request, user)
 
-    def get(self, request):
-        form = LoginForm()
-        context = {"form": form}
-        return render(request, "users/login.html", context)
-
-    def post(self, request):
-        form = LoginForm(request.POST)
-        context = {"form": form}
-        if form.is_valid():
-            username = form.cleaned_data.get("username", "")
-            password = form.cleaned_data.get("password", "")
-            user = authenticate(username=username, password=password)
-
-            if user:
-                django_login(request, user)
-                url = request.GET.get("next", "login")
-                return redirect(url)
-
-            else:
-                context["error"] = "Usuario o contraseña incorrectos"
-
-        return render(request, "users/login.html", context)
 
 
 def logout(request):
     django_logout(request)
-    return reverse_lazy("/")
+    return redirect("home")
 
 
 class SignupView(FormView):
@@ -53,4 +39,4 @@ class SignupView(FormView):
 
         django_login(self.request, user)
 
-        return redirect("/")
+        return redirect("home")
